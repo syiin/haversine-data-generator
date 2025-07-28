24b3ba58087eded6ae13281c4e4d2691db394cfb
syiin <xbio.oo@gmail.com>
2025-07-28 21:48:12 +0800

    remove redundant json primitive enum, replace with simpler just json value and calculate distance pairs

---
+++ src/haversine.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::{ Write, BufWriter };
+use crate::generator::{ Pair };
+
+const DEGREES_TO_RADIANS: f64 = 0.01745329251994329577;
+
+#[inline]
+fn radians_from_degrees(degrees: f64) -> f64 {
+    DEGREES_TO_RADIANS * degrees
+}
+
+#[inline]
+pub fn reference_haversine(pair: &Pair, earth_radius: f64) -> f64 {
+    let mut lat0 = pair.x0;
+    let lng0 = pair.y0;
+    let mut lat1 = pair.x1;
+    let lng1 = pair.y1;
+
+    let d_lat = radians_from_degrees(lat1 - lat0);
+    let d_lon = radians_from_degrees(lng1 - lng0);
+    lat0 = radians_from_degrees(lat0);
+    lat1 = radians_from_degrees(lat1);
+
+    let a = (d_lat / 2.0).sin().powi(2) + lat0.cos() * lat1.cos() * (d_lon / 2.0).sin().powi(2);
+    let c = 2.0 * a.sqrt().asin();
+
+    earth_radius * c
+}
+
+
+pub fn save_run_metrics(distances: &Vec<f64>, seed: u64, num_pairs: u64, cumu_distance: f64) -> Result<(), Box<dyn std::error::Error>> {
+    { 
+        let mut file = File::create("haversine_metrics.txt")?;
+        writeln!(file, "Seed: {}", seed)?;
+        writeln!(file, "Points: {}", num_pairs)?;
+        writeln!(file, "Est Distance: {}", cumu_distance)?;
+    }
+
+    {
+        let file = File::create("haversine.f64")?;
+        let mut writer = BufWriter::new(file);
+        for &distance in distances {
+            writer.write_all(&distance.to_le_bytes())?;
+        }
+    }
+
+    println!("Seed: {}", seed);
+    println!("Points: {}", num_pairs);
+    println!("Est Distance: {}", cumu_distance);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,58 @@
 mod generator;
 mod parser;
 mod lexer;
+mod haversine;
 
 use rand::rngs::SmallRng;
 use rand::SeedableRng;
 use generator::{ Pairs, Pair };
-use generator::{ reference_haversine, save_run_metrics };
+use haversine::{ reference_haversine, save_run_metrics };
 use lexer::{ parse_file };
-use parser::{ parse_tokens, format_json };
+use parser::{ parse_tokens, JsonValue };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     generate_pairs()?;
     let file = std::fs::File::open("haversine.json")?;
     let tokens = parse_file(file);
-    // println!("Token length: {}", tokens.len());
-    // for token in &tokens {
-    //     println!("{}", token.format());
-    // }
+
     let json = parse_tokens(&tokens);
-    let formatted = format_json(json.as_ref().unwrap());
-    println!("{}", formatted);
+    if let Some(json_value) = json {
+        let distances = calculate_pairs(json_value);
+        println!("Distances: {:?}", distances);
+    } else {
+        println!("Error parsing JSON");
+    }
     
     Ok(())
+}
+
+fn calculate_pairs(json: JsonValue) -> Vec<f64> {
+    let JsonValue::Object(map) = json else { return Vec::new(); };
+    let JsonValue::Array(pairs_array) = map.get("pairs").expect("Error getting pairs array") else { return Vec::new(); };
+
+    let mut distances: Vec<f64> = Vec::new();
+    for pair in pairs_array {
+        let JsonValue::Object(pair_map) = pair else { continue; };
+        let x0 = pair_map.get("x0").unwrap();
+        let y0 = pair_map.get("y0").unwrap();
+        let x1 = pair_map.get("x1").unwrap();
+        let y1 = pair_map.get("y1").unwrap();
+
+        let pair = Pair::new(
+            get_number_from_json(x0), 
+            get_number_from_json(y0), 
+            get_number_from_json(x1), 
+            get_number_from_json(y1)
+        );
+        let distance = reference_haversine(&pair, 6372.8);
+        distances.push(distance);
+    }
+    return distances;
+}
+
+fn get_number_from_json(json: &JsonValue) -> f64 {
+    let JsonValue::Number(n) = json else { return 0.0; };
+    return *n;
 }
 
 fn generate_pairs() -> Result<(), Box<dyn std::error::Error>> {
