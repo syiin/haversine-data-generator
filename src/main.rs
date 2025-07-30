240d4fd08fe8682593e32c59fbc69c636e5f673b
syiin <xbio.oo@gmail.com>
2025-07-30 19:10:39 +0800

    profile reading and parsing json

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod generator;
 mod parser;
 mod lexer;
 mod haversine;
+mod timer;
 
 use clap::{Parser, Subcommand, Arg};
 use rand::rngs::SmallRng;
@@ -10,6 +11,7 @@ use generator::{ Pairs, Pair };
 use haversine::{ reference_haversine, save_run_metrics, read_run_metrics };
 use lexer::{ parse_file };
 use parser::{ parse_tokens, JsonValue };
+use timer::{ read_cpu_timer };
 
 
 
@@ -51,6 +53,12 @@ enum Command {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let prof_begin = unsafe { read_cpu_timer() };
+    let mut prof_read = 0;
+    let mut prof_misc_setup = 0;
+    let mut prof_sum = 0;
+    let mut prof_misc_output = 0;
+
     let cli = Cli::parse();
 
     match &cli.command {
@@ -58,26 +66,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             generate_pairs(output_file, metrics_output, distance_output)?;
         },
         Some(Command::Calculate { input_file, metrics_file }) => {
+            prof_read = unsafe { read_cpu_timer() };
+
             let file = std::fs::File::open(input_file)?;
             let (seed, points, est_distance) = read_run_metrics(metrics_file)?;
+            
+            prof_misc_setup = unsafe { read_cpu_timer() };
+
             let tokens = parse_file(file);
 
             println!("Seed: {}", seed);
             println!("Points: {}", points);
             println!("Est Distance: {}", est_distance);
 
+            let prof_parse = unsafe { read_cpu_timer() };
             let json = parse_tokens(&tokens);
             if let Some(json_value) = json {
                 let distances: Vec<f64> = calculate_pairs(json_value);
+                
+                prof_sum = unsafe { read_cpu_timer() };
                 let actual_distance: f64 = distances.iter().sum();
 
+                prof_misc_output = unsafe { read_cpu_timer() };
                 println!("Actual Distance: {}", actual_distance);
                 println!("Expected Distance: {}", est_distance);
                 println!("Distance Difference: {}", (actual_distance - est_distance).abs());
-
             } else {
                 println!("Error parsing JSON");
             }
+
+            let prof_end = unsafe { read_cpu_timer() };
+            let total_cpu_elapsed = prof_end - prof_begin;
+
+            print_time_elapsed("Startup", total_cpu_elapsed, prof_begin, prof_read);
+            print_time_elapsed("Read", total_cpu_elapsed, prof_read, prof_misc_setup);
+            print_time_elapsed("Misc Setup", total_cpu_elapsed, prof_misc_setup, prof_parse);
+            print_time_elapsed("Parse", total_cpu_elapsed, prof_parse, prof_sum);
+            print_time_elapsed("Sum", total_cpu_elapsed, prof_sum, prof_misc_output);
+            print_time_elapsed("Misc Output", total_cpu_elapsed, prof_misc_output, prof_end);
         },
         None => {
             println!("Haversine Data Generator");
@@ -156,4 +182,10 @@ fn generate_pairs(file_path: &str, metrics_output: &str, distance_output: &str) 
     let _ = save_run_metrics(&distances, seed, num_pairs, cumu_distance, metrics_output, distance_output);
 
     Ok(())
+}
+
+fn print_time_elapsed(label: &str, total_cpu_elapsed: u64, prof_begin: u64, prof_end: u64) {
+    let elapsed: f64 = prof_end as f64 - prof_begin as f64;
+    let percent: f64 = 100.0 * (elapsed / total_cpu_elapsed as f64);
+    println!("{}: {}, ({}%)", label, elapsed, percent);
 }
